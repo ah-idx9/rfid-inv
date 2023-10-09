08a24fda5e44e07145232bd51e6872c1f1b32c0f
DELL <prakashabhishek32@gmail.com>
2023-10-09 18:06:26 +0530

    Fixed: User can add manully RFID Queue

--- rfid/rfid/doctype/rfid_print_queue/rfid_print_queue.js
+++ rfid/rfid/doctype/rfid_print_queue/rfid_print_queue.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2023, RFID and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('RFID Print Queue', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on('RFID Print Queue',
+	"onload", function(frm) {
+		frm.set_query("item_code", function() {
+			return {
+				"filters": {
+					"has_variants" : 0
+				}
+			};
+		});
 });
