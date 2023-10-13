b8de7254bf1375701db67c9d30055f046d72eec2
DELL <prakashabhishek32@gmail.com>
2023-10-13 18:00:41 +0530

    Added: Default has_serial, add rfid by se & sn

--- rfid/rfid/doctype/item/item.py
+++ rfid/rfid/doctype/item/item.py
@@ -5,3 +5,6 @@ def before_save(doc,method=None):
     ascii_list = list(itertools.chain(*map(lambda x: map(ord, x), doc.item_code)))
     ascii_list_new = (''.join(map(str, ascii_list)))
     doc.custom_barcode = ascii_list_new
+    if doc.serial_no_series == None:
+        doc.serial_no_series = doc.item_code + '.######'
+        
--- rfid/rfid/doctype/stock_entry/stock_entry.py
+++ rfid/rfid/doctype/stock_entry/stock_entry.py
@@ -1,11 +1,18 @@
 import frappe
 import itertools
+import hashlib
 
 def on_submit(doc,method=None):
-    item_code,name = frappe.db.get_value('Serial No',{'purchase_document_no':doc.name},['item_code','name'])
-    serial_item_code = item_code+name
-    ascii_list = list(itertools.chain(*map(lambda x: map(ord, x), serial_item_code)))
-    ascii_list_new = (''.join(map(str, ascii_list)))
+    serials = frappe.db.get_list('Serial No',{'purchase_document_no':doc.name}, pluck='name')
+    for serial in serials:
+        input_string = serial
+        # Calculate the SHA-256 hash of the input string
+        sha256_hash = hashlib.sha256(input_string.encode()).hexdigest()
 
-    frappe.db.set_value('Serial No',{'purchase_document_no':doc.name},'custom_barcode',ascii_list_new)
+        # Ensure the output is up to 10 characters
+        output_length = min(len(sha256_hash), 10)
+        rfid_data = sha256_hash[:output_length]
+
+
+        frappe.db.set_value('Serial No',serial,'custom_barcode',rfid_data)
     frappe.db.commit()
